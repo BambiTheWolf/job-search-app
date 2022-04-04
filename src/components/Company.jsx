e4cb2bbce9c5f23aec018087f1eb81556866b9cb
Michael Redruello <redruellomichael@gmail.com>
2022-04-04 16:37:42 +0200

    job-search-app almost complete

---
+++ src/components/Company.jsx
@@ -0,0 +1,5 @@
+import { useParams } from "react-router-dom";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+const Company = () => {};
