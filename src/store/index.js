ba57d98966fe052c87ad4fe5e70607ad7581b01a
Michael Redruello <redruellomichael@gmail.com>
2022-04-07 16:11:25 +0200

    ex 1 completed

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import mainReducer from "./reducers";
 
-export default createStore(mainReducer);
+import thunk from "redux-thunk";
+
+const composeFunctionThatAlwaysWorks =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {
+  favourites: [],
+};
+
+export default createStore(
+  mainReducer,
+  initialState,
+  composeFunctionThatAlwaysWorks(applyMiddleware(thunk))
+);
