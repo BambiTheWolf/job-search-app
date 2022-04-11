5be22260cf19c4441656083dee77b896e03539e9
Michael Redruello <redruellomichael@gmail.com>
2022-04-11 17:00:05 +0200

    hw completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { configureStore, persistor } from "./store";
 import MainSearch from "./components/MainSearch";
 import CompanySearchResults from "./components/CompanySearchResults";
 import Favourites from "./components/Favourites";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import store from "./store";
-import { Provider } from "react-redux";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainSearch />} />
-          <Route path="/favourites" element={<Favourites />} />
-          <Route path="/:companyName" element={<CompanySearchResults />} />
-        </Routes>
-      </BrowserRouter>
+    <Provider store={configureStore}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainSearch />} />
+            <Route path="/favourites" element={<Favourites />} />
+            <Route path="/:companyName" element={<CompanySearchResults />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,12 @@ import thunk from "redux-thunk";
 import favouritesReducer from "./reducers/favourites";
 import jobsReducer from "./reducers/jobs";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const initialState = {
   favourites: {
     elements: [],
@@ -12,15 +18,30 @@ export const initialState = {
   },
 };
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const mainReducer = combineReducers({
   favourites: favouritesReducer,
   jobs: jobsReducer,
 });
 
-export default createStore(
-  mainReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+  transform: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY,
+      onError: (error) => {
+        console.log(error);
+      },
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
+export const configureStore = createStore(
+  persistReducer,
   initialState,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(configureStore);
